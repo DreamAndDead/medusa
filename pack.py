d23233df35ebe6022f2166de41ab48b5b305312d
DreamAndDead <favorofife@yeah.net>
2019-09-24 11:27:10 +0800

    attach version to exe

--- pack.py
+++ pack.py
@@ -2,9 +2,11 @@ import PyInstaller.__main__
 
 pack_name = 'py2lua'
 pack_file = '%s.py' % pack_name
+version_file = 'exe_version.txt'
 
 PyInstaller.__main__.run([
-    '--name=%s' % 'py2lua',
+    '--name=%s' % pack_name,
     '--onefile',
+	'--version-file=%s' % version_file,
     pack_file
 ])
